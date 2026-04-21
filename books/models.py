fca576bfa20456f84a771a130285b4d375931764
acxa <anavillavillaana@gmail.com>
2026-04-20 22:13:17 -0700

    lologreeee

---
+++ books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    image = models.CharField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
